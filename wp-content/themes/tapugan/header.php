0943aa012599533ca8d07b504012a690fe5e96b1
aleksandrnikolajcuk <deathstars111>
2016-09-06 09:06:29 +0300

    Correctness

--- wp-content/themes/tapugan/header.php
+++ wp-content/themes/tapugan/header.php
@@ -6,99 +6,11 @@
     <meta name="format-detection" content="address=no">
     <meta charset="UTF-8">
 
-    <title><?php document_title(); ?></title>
+    <title><?php wp_title(''); ?></title>
     <?php wp_head(); ?>
 
     <link rel="shortcut icon" href="<?php echo TEMPLATEURI; ?>/favicon.ico" />
-
-
-
-<?php
-
-if(is_page_template('page-contact.php')): ?>
-<!--    <script type="text/javascript">-->
-<!--        function isEmpty() {-->
-<!--            strfield1 = document.forms[0].name1.value;-->
-<!--            strfield2 = document.forms[0].company.value;-->
-<!--            strfield3 = document.forms[0].email.value;-->
-<!--            strfield4 = document.forms[0].phone.value;-->
-<!--            strfield5 = document.forms[0].msg.value;-->
-<!--            var name;-->
-<!--            var last;-->
-<!--            var phone;-->
-<!--            var mail;-->
-<!--            var comment = true;-->
-<!--            var stripped = strfield4.replace(/[\(\)\.\-\ ]/g, '');-->
-<!--            validRegExp = /^[^@]+@[^@]+.[a-z]{2,}$/i;-->
-<!--            strEmail = document.forms[0].email.value;-->
-<!--            if (strfield1 == "" || strfield1 == null || !isNaN(strfield1) || strfield1.charAt(0) == ' ')-->
-<!--            {-->
-<!--                ChangeTitle('name_error');-->
-<!--                name=false;-->
-<!--            }else{-->
-<!--                name=true;-->
-<!--                elemTitle('name_error');-->
-<!--            }-->
-<!--            if (strfield2 == "" || strfield2 == null || !isNaN(strfield2) || strfield2.charAt(0) == ' ')-->
-<!--            {-->
-<!--                ChangeTitle('last_error');-->
-<!--                last=false;-->
-<!--            }else{-->
-<!--                last=true;-->
-<!--                elemTitle('last_error');-->
-<!--            }-->
-<!--            if (strEmail.search(validRegExp) == -1)-->
-<!--            {-->
-<!--                ChangeTitle('mail_error');-->
-<!--                mail=false;-->
-<!--            } else{-->
-<!--                mail=true;-->
-<!--                elemTitle('mail_error');-->
-<!--            }-->
-<!---->
-<!--            if (strfield4 == "" || strfield4 == null || strfield4.charAt(0) == ' ')-->
-<!--            {-->
-<!--                ChangeTitle('phone_error');-->
-<!--                phone=false;-->
-<!--            }else{-->
-<!--                phone=true;-->
-<!--                elemTitle('phone_error');-->
-<!--            }-->
-<!---->
-<!--            if(name==true && last==true && phone==true && mail==true && comment==true){-->
-<!--                return true;-->
-<!--            }else {-->
-<!--                return false;-->
-<!--            }-->
-<!--        }-->
-<!--        function check(form){-->
-<!--            if (isEmpty()){-->
-<!--                document.getElementById('tofes').action="form.aspx";-->
-<!--                return true;-->
-<!--            }-->
-<!--            return false;-->
-<!--        }-->
-<!--        function ChangeTitle(whatToWrite) {-->
-<!---->
-<!--            var head1 = document.getElementById(whatToWrite);-->
-<!--            head1.style.visibility = "visible";-->
-<!--        }-->
-<!--        function elemTitle(whatToWrite) {-->
-<!---->
-<!--            var head1 = document.getElementById(whatToWrite);-->
-<!--            head1.style.visibility = "hidden";-->
-<!--        }-->
-<!--        function hideAll(){-->
-<!--            document.getElementById("name_error").style.visibility = "hidden";-->
-<!--            document.getElementById("last_error").style.visibility = "hidden";-->
-<!--            document.getElementById("mail_error").style.visibility = "hidden";-->
-<!--            document.getElementById("phone_error").style.visibility = "hidden";-->
-<!--        }-->
-<!--    </script>-->
-<?php endif;
-?>
-
-
+    
 </head>
 <body>
 <?php if (is_page() || is_single() || is_singular() || is_404() || is_tax() || is_category() || is_tax() ) {
